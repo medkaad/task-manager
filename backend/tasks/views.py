d54f725ec8e4f84c673c9f0745979991016566c7
medkaad <kardalm132@gmail.com>
2025-12-19 23:03:47 +0100

    Frontend: setup Vue.js with Axios and display Project list from API

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
---
+++ frontend/src/components/ProjectList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h2>Projects</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'ProjectList',
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:8000/api/projects/')
+      .then(res => this.projects = res.data)
+      .catch(err => console.error(err))
+  }
+}
+</script>
