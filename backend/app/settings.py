fd8dfc9910a23079d44fb3cb511317b76f3d78b7
medkaad <kardalm132@gmail.com>
2025-12-20 10:04:35 +0100

    Backend: add JWT authentification with DFR

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -131,3 +131,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
