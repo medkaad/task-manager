3243f5681effb92875baa725d191791f8ce7c743
medkaad <kardalm132@gmail.com>
2025-12-20 10:08:39 +0100

    Backend: project Project and Task APIs with JWT authentication

--- backend/tasks/views.py
+++ backend/tasks/views.py
@@ -1,11 +1,14 @@
 from rest_framework import viewsets
 from .models import Project, Task
 from .serializers import ProjectSerializer, TaskSerializer
+from rest_framework.permission import IsAuthenticated
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
 
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [IsAuthenticated]
